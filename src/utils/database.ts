e9e36e1ef05331b69ae9ea221efc26a4a45f82e2
JulianSchumacher99 <julian.schumacher@sterntaler.com>
2021-10-29 11:20:38 +0200

    ✨post and read character

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -6,3 +6,7 @@ export async function connectDatabase(url: string) {
   client = new MongoClient(url);
   await client.connect();
 }
+
+export function getCharactersCollection() {
+  return client.db().collection('Characters');
+}
